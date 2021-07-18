a3f84b7b5f768b112a51750f1223926f9079ac47
MountainNine <goosan126@gmail.com>
2021-07-18 11:39:57 +0900

    시간 초과

---
+++ boggle.py
@@ -0,0 +1,65 @@
+dx = [-1, 0, 1, -1, 1, -1, 0, 1]
+dy = [-1, -1, -1, 0, 0, 1, 1, 1]
+
+
+def search(starty, startx, checkpos, split_word, boggle):
+    for i in range(8):
+        if 0 <= starty + dy[i] < 5 and 0 <= startx + dx[i] < 5:
+            if boggle[starty + dy[i]][startx + dx[i]] == split_word[checkpos]:
+                if checkpos == len(split_word) - 1:
+                    return True
+                elif search(starty + dy[i], startx + dx[i], checkpos + 1, split_word, boggle):
+                    return True
+
+    return False
+
+
+def check_word(boggle, word):
+    for i in range(5):
+        for j in range(5):
+            w = list(word)
+            if boggle[i][j] == w[0]:
+                if(search(i,j,1,w,boggle)):
+                    print(str.format("{} {}", word, "YES"))
+                    return
+    print(str.format("{} {}", word, "NO"))
+
+
+c = input()
+boggle = []
+for i in range(int(c)):
+    for j in range(5):
+        boggle_row = input()
+        boggle.append(list(boggle_row))
+
+    n = input()
+    word = []
+    for j in range(int(n)):
+        word_row = input()
+        word.append(word_row)
+    for w in word:
+        check_word(boggle, w)
+
+# 1
+# NNNNS
+# NEEEN
+# NEYEN
+# NEEEN
+# NNNNN
+# 2
+# YES
+# SEVEN
+
+# 1
+# URLPM
+# XPRET
+# GIAET
+# XTNZY
+# XOQRS
+# 6
+# PRETTY
+# GIRL
+# REPEAT
+# KARA
+# PANDORA
+# GIAZAPX
