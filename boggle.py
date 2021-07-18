b6400860499ed8561a540879d525e0c362c3a6b2
MountainNine <goosan126@gmail.com>
2021-07-18 12:54:07 +0900

    보류

--- boggle.py
+++ boggle.py
@@ -3,15 +3,17 @@ dy = [-1, -1, -1, 0, 0, 1, 1, 1]
 
 
 def search(starty, startx, checkpos, split_word, boggle):
+    cache = [[[-1 for x in range(5)] for y in range(5)] for z in range(10)]
     for i in range(8):
         if 0 <= starty + dy[i] < 5 and 0 <= startx + dx[i] < 5:
             if boggle[starty + dy[i]][startx + dx[i]] == split_word[checkpos]:
                 if checkpos == len(split_word) - 1:
-                    return True
-                elif search(starty + dy[i], startx + dx[i], checkpos + 1, split_word, boggle):
-                    return True
-
-    return False
+                    return 1
+                ret = cache[checkpos][starty][startx]
+                if ret != -1:
+                    return ret
+                cache[checkpos][starty][startx] = search(starty + dy[i], startx + dx[i], checkpos + 1, split_word, boggle)
+    return cache[checkpos][starty][startx]
 
 
 def check_word(boggle, word):
@@ -19,7 +21,7 @@ def check_word(boggle, word):
         for j in range(5):
             w = list(word)
             if boggle[i][j] == w[0]:
-                if(search(i,j,1,w,boggle)):
+                if search(i, j, 1, w, boggle) == 1:
                     print(str.format("{} {}", word, "YES"))
                     return
     print(str.format("{} {}", word, "NO"))
