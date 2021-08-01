b74958904606044af3552a3fafbfcc235f503b6c
MountainNine <goosan126@gmail.com>
2021-08-01 12:15:05 +0900

    q1929 풀이 완료

--- math_2/q1929.py
+++ math_2/q1929.py
@@ -1,17 +1,12 @@
 m, n = input().split()
 m, n = int(m), int(n)
 
-result = []
-list_num = list(range(m, n + 1))
-r = int(n ** (1 / 2))
 for i in range(m, n + 1):
+    flag = True
     r = int(i ** (1 / 2))
-    f = False
     for j in range(2, r + 1):
-        if i % j == 0 and i != j:
-            f = True
+        if i % j == 0:
+            flag = False
             break
-        elif i % j == 0 and i == j:
-            print(i)
-    if not f and i != 1:
+    if flag and i != 1:
         print(i)
